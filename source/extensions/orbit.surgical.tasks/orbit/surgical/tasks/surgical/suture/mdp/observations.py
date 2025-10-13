3e2da7174c0ba21967af7e743d2555c0d19fbd96
jan.kranjec12 <jan.kranjec12@gmail.com>
2025-10-13 16:08:39 +0200

    added mdp

---
+++ source/extensions/orbit.surgical.tasks/orbit/surgical/tasks/surgical/suture/mdp/observations.py
@@ -0,0 +1,63 @@
+# Copyright (c) 2024, The ORBIT-Surgical Project Developers.
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+from __future__ import annotations
+
+import torch
+from typing import TYPE_CHECKING
+
+from isaaclab.assets import RigidObject
+from isaaclab.managers import SceneEntityCfg
+from isaaclab.sensors import FrameTransformer
+from isaaclab.assets import Articulation
+from isaaclab.utils.math import subtract_frame_transforms
+
+if TYPE_CHECKING:
+    from isaaclab.envs import ManagerBasedRLEnv
+
+
+def ee_frame_pos(env: ManagerBasedRLEnv, ee_frame_cfg: SceneEntityCfg = SceneEntityCfg("ee_frame")) -> torch.Tensor:
+    ee_frame: FrameTransformer = env.scene[ee_frame_cfg.name]
+    ee_frame_pos = ee_frame.data.target_pos_w[:, 0, :] - env.scene.env_origins[:, 0:3]
+
+    return ee_frame_pos
+
+
+def ee_frame_quat(env: ManagerBasedRLEnv, ee_frame_cfg: SceneEntityCfg = SceneEntityCfg("ee_frame")) -> torch.Tensor:
+    ee_frame: FrameTransformer = env.scene[ee_frame_cfg.name]
+    ee_frame_quat = ee_frame.data.target_quat_w[:, 0, :]
+
+    return ee_frame_quat
+
+def gripper_pos(
+    env: ManagerBasedRLEnv,
+    finger1_name: str = "tool_yaw1",
+    finger2_name: str = "tool_yaw2", 
+    robot_cfg: SceneEntityCfg = SceneEntityCfg("robot")
+) -> torch.Tensor:
+    
+    robot: Articulation = env.scene[robot_cfg.name]
+
+    finger1_idx = robot.joint_names.index(finger1_name)
+    finger2_idx = robot.joint_names.index(finger2_name)
+
+    finger_joint_1 = robot.data.joint_pos[:, finger1_idx].clone().unsqueeze(1)
+    finger_joint_2 = -1 * robot.data.joint_pos[:, finger2_idx].clone().unsqueeze(1)
+
+    return torch.cat((finger_joint_1, finger_joint_2), dim=1)
+
+def object_position_in_robot_root_frame(
+    env: ManagerBasedRLEnv,
+    robot_cfg: SceneEntityCfg = SceneEntityCfg("robot"),
+    object_cfg: SceneEntityCfg = SceneEntityCfg("object"),
+) -> torch.Tensor:
+    """The position of the object in the robot's root frame."""
+    robot: RigidObject = env.scene[robot_cfg.name]
+    object: RigidObject = env.scene[object_cfg.name]
+    object_pos_w = object.data.root_pos_w[:, :3]
+    object_pos_b, _ = subtract_frame_transforms(
+        robot.data.root_state_w[:, :3], robot.data.root_state_w[:, 3:7], object_pos_w
+    )
+    return object_pos_b
