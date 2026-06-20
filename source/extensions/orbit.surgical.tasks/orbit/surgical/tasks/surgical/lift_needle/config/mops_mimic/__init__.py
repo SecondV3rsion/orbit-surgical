7aa00c5c8ca3f3c3d8157865d50bef9b5405af24
SecondV3rsion <jan.kranjec@gmail.com>
2026-06-20 11:40:07 +0200

    adding clip to rel task

--- source/extensions/orbit.surgical.tasks/orbit/surgical/tasks/surgical/lift_needle/config/mops_mimic/__init__.py
+++ source/extensions/orbit.surgical.tasks/orbit/surgical/tasks/surgical/lift_needle/config/mops_mimic/__init__.py
@@ -47,7 +47,7 @@ gym.register(
     entry_point="isaaclab.envs:ManagerBasedRLEnv",
     kwargs={
         "env_cfg_entry_point": ik_rel_env_cfg.NeedleLiftEnvCfg,
-        "robomimic_bc_cfg_entry_point": os.path.join(agents.__path__[0], "robomimic/bc_validation.json"),
+        "robomimic_bc_cfg_entry_point": os.path.join(agents.__path__[0], "robomimic/bc_rnn_low_dim.json"),
     },
     disable_env_checker=True,
 )
--- source/extensions/orbit.surgical.tasks/orbit/surgical/tasks/surgical/lift_needle/config/mops_mimic/ik_rel_env_cfg.py
+++ source/extensions/orbit.surgical.tasks/orbit/surgical/tasks/surgical/lift_needle/config/mops_mimic/ik_rel_env_cfg.py
@@ -57,6 +57,7 @@ class NeedleLiftEnvCfg(LiftEnvCfg):
             body_name="tool_tcp0",
             controller=DifferentialIKControllerCfg(command_type="pose", use_relative_mode=True, ik_method="dls"),
             scale=1.0,
+            clip={"kuka_A(1|2|3|4|5|6)": (-0.01, 0.01)},
         )
         self.actions.finger_joint_pos = mdp.BinaryJointPositionActionCfg(
             asset_name="robot",
